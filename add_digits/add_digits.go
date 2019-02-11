bc32798d300e1725092bb8b9ebe6f69ac612b99f
vidya <vsagarj@gmail.com>
2019-02-10 22:01:52 -0800

    improve formatting of comments

--- add_digits/add_digits.go
+++ add_digits/add_digits.go
@@ -1,27 +1,29 @@
-// https://leetcode.com/problems/add-digits/
+/*
+	https://leetcode.com/problems/add-digits/
 
-// 258. Add Digits
+	258. Add Digits
 
-// Given a non-negative integer num, repeatedly add all its digits until
-// the result has only one digit.
+	Given a non-negative integer num, repeatedly add all its digits until
+	the result has only one digit.
 
-// Example:
+	Example:
+	Input: 38
+	Output: 2
 
-// Input: 38
-// Output: 2
-// Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
-//              Since 2 has only one digit, return it.
-// Follow up:
-// Could you do it without any loop/recursion in O(1) runtime?
+	Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
+				Since 2 has only one digit, return it.
+*/
+
 
 package main
 
-// AddDigits():
-// add digits of num. repeat the process till you are left with
-// a single digit.
-//
-// return the single digit result
-//
+/*
+	AddDigits():
+	add digits of num. repeat the process till you are left with
+	a single digit.
+
+	return the single digit result
+*/
 func AddDigits(num int) int {
 	sumOnce := func(num int) int {
 		n, sum := num, 0
