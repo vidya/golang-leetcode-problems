67326d24caafe222300de44dfdb42a8515c635f0
vidya <vsagarj@gmail.com>
2019-02-13 15:06:42 -0800

    simplify naming

--- DecodeString/decode_string.go
+++ DecodeString/decode_string.go
@@ -1,4 +1,58 @@
+Last login: Sun Feb 10 13:47:41 on ttys023
+>pwd
+/Users/vidya
+>cd Documents/Project/*Go/Git*
+>cd golang-leetcode-problems
+>git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+	DecodeString/
+
+nothing added to commit but untracked files present (use "git add" to track)
+>git add DecodeString/
+>git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes to be committed:
+  (use "git reset HEAD <file>..." to unstage)
+
+	new file:   DecodeString/decode_string.go
+	new file:   DecodeString/decode_string_test.go
+
+>git commit -m "solution to DecodeString problem" -a
+[master 4f295cd] solution to DecodeString problem
+ 2 files changed, 225 insertions(+)
+ create mode 100644 DecodeString/decode_string.go
 /*
+        https://leetcode.com/problems/decode-string/
+
+        394. Decode String
+
+        Given an encoded string, return it's decoded string.
+
+        The encoding rule is: k[encoded_string], where the encoded_string
+        inside the square brackets is being repeated exactly k times.
+        Note that k is guaranteed to be a positive integer.
+
+        You may assume that the input string is always valid; No extra
+        white spaces, square brackets are well-formed, etc.
+
+        Furthermore, you may assume that the original data does not
+        contain any digits and that digits are only for those repeat
+        numbers, k. For example, there won't be input like 3a or 2[4].
+
+        Examples:
+        s = "3[a]2[bc]", return "aaabcbc".
+        s = "3[a2[c]]", return "accaccacc".
+        s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+*/
+
+"DecodeString/decode_string.go" 180L, 3776C/*
 	https://leetcode.com/problems/decode-string/
 
 	394. Decode String
